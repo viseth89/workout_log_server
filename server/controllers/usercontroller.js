62537f465cb4ec29a85451feb8621d86bcd418e4
viseth89 <viseth89@gmail.com>
2021-06-30 13:16:58 -0500

    added JWT

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,19 +1,22 @@
 const router = require("express").Router();
 const { UserModel } = require("../models");
 const { UniqueConstraintError } = require("sequelize/lib/errors");
+const jwt = require("jsonwebtoken")
 
 router.post("/register", async (req, res) => {
   let { email, password } = req.body.user;
-
   try {
     const User = await UserModel.create({
       email,
       password,
     });
 
+    let token = jwt.sign({id: User.id, email: User.email}, "i_am_secret", {expiresIn: 60 * 60 * 24});
+
     res.status(201).json({
       message: "User successfully registered",
       user: User,
+      sessionToken: token
     });
   } catch (err) {
     if (err instanceof UniqueConstraintError) {
@@ -39,9 +42,13 @@ router.post("/login", async (req, res) => {
     });
 
     if (loginUser) {
+
+        let token = jwt.sign({id: loginUser.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
+
       res.status(200).json({
         user: loginUser,
         message: "User successfully logged in!",
+        sessionToken: token
       });
     } else {
       res.status(401).json({
