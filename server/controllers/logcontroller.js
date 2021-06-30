e71163f4f335dcf15952ad0dac14c178a0ecb2f1
viseth89 <viseth89@gmail.com>
2021-06-30 16:16:06 -0500

    Added update and delete -> debugging update

--- server/controllers/logcontroller.js
+++ server/controllers/logcontroller.js
@@ -26,8 +26,86 @@ router.post("/create", validateJWT, async(req, res) => {
     LogModel.create(logEntry)
 })
 
-router.get('/about', (req, res) => {
-    res.send('This is the about Route!')
+router.get('/', async (req, res) => {
+ try {
+     const entries = await LogModel.findAll();
+     res.status(200).json(entries);
+
+ } catch(err) {
+     res.status(500).json({error:err})
+ }
 })
 
+router.get('/mine', validateJWT, async(req, res) => {
+    const {id} = req.user;
+ try {
+     const userLogs = await LogModel.findAll({
+         where : {
+             owner_id: id
+         }
+     });
+
+     res.status(200).json(userLogs);
+
+ } catch(err) {
+     res.status(500).json({error:err})
+ }
+})
+
+router.get('/:description', async(req,res) => {
+    const { description } = req.params;
+    try {
+        const results = await LogModel.findAll({
+            where: { description: description }
+        });
+        res.status(200).json(results);
+    } catch(err) {
+        res.status(500).json({ error: err})
+    }
+})
+
+router.put("/update/:entryId", validateJWT, async (req, res) => {
+    const { description, definition, result } = req.body.log
+    const logId = req.params.entryId;
+    const userId = req.user.id;
+
+    const query = {
+        where: {
+            id: logId,
+            owner_id: userId
+        }
+    };
+
+    const updatedLog = {
+        description: description,
+        definition: definition,
+        result: result
+    };
+
+    try {
+        const update = await LogModel.update(updatedLog, query);
+        res.status(200).json(update);
+    } catch(err) {
+        res.status(500).json({ error: err })
+    }
+})
+
+router.delete('/delete/:id', validateJWT, async(req, res) => {
+    const ownerId = req.user.id;
+    const logId = req.params.id
+
+    try {
+        const query = {
+            where: {
+                id: journalId,
+                owner: ownerId
+            }
+        };
+
+        await LogModel.destroy(query);
+        res.status(200).json({ message: "Log Entry Removed"})
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+})
 module.exports = router;
